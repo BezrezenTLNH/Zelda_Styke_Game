e5e36e35bd673147c017dde05f7adaef7b3ef370
Mikhail Zherebtsov <bezrezen97@gmail.com>
2023-11-13 11:27:29 +0100

    make a visible rocks position

--- engine/main.py
+++ engine/main.py
@@ -7,9 +7,11 @@ class Game:
     def __init__(self):
         # general setup
         pygame.init()
-        self.screen = pygame.display.set_mode((WIDTH,HEIGHT))
+        self.screen = pygame.display.set_mode((WIDTH, HEIGTH))
         pygame.display.set_caption('Zelda')
         self.clock = pygame.time.Clock()
+
+        self.level = Level()
 
     def run(self):
         while True:
@@ -19,6 +21,7 @@ class Game:
                     sys.exit()
 
                 self.screen.fill('black')
+                self.level.run()
                 pygame.display.update()
                 self.clock.tick(FPS)
 
