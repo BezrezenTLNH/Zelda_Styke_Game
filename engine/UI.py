05af6cafa15f08e040adc7a6ee0ff325531e295a
Mikhail Zherebtsov <bezrezen97@gmail.com>
2023-11-29 10:20:50 +0100

    add a selection box display

--- engine/UI.py
+++ engine/UI.py
@@ -33,10 +33,18 @@ class UI:
         y = self.display_surface.get_size()[1] - 20
         text_rect = text_surface.get_rect(bottomright=(x, y))
 
+        pygame.draw.rect(self.display_surface, UI_BG_COLOR, text_rect.inflate(20, 20))
         self.display_surface.blit(text_surface, text_rect)
+        pygame.draw.rect(self.display_surface, UI_BORDER_COLOR, text_rect.inflate(20, 20), 3)
+
+    def selection_box(self, left, top):
+        bg_rect = pygame.Rect(left, top, ITEM_BOX_SIZE, ITEM_BOX_SIZE)
+        pygame.draw.rect(self.display_surface, UI_BG_COLOR, bg_rect)
+        pygame.draw.rect(self.display_surface, UI_BORDER_COLOR, bg_rect, 3)
 
     def display(self, player):
         self.show_bar(player.health, player.stats['health'], self.health_bar_rect, HEALTH_COLOR)
         self.show_bar(player.energy, player.stats['energy'], self.energy_bar_rect, ENERGY_COLOR)
 
         self.show_exp(player.exp)
+        self.selection_box(10, 630)
